c5d262c6e7381aff34915e11a02d9dbabc7540ca
takumimi1124 <takufrom1124@gmail.com>
2021-01-17 23:30:19 +0900

    created top-page

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from "react"
+import styled from "styled-components"
+
+const Wrapper = styled.footer`
+  background: #999;
+  padding: 1em;
+  color: white;
+`
+
+const Footer = () => (
+  <Wrapper>2021 たくあや</Wrapper>
+)
+
+export default Footer
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,85 @@
 import React from "react"
-import { Link } from "gatsby"
+import { graphql, Link } from "gatsby"
+import styled from "styled-components"
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import Header from "../components/Header"
+import Footer from "../components/Footer"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hello! Gatsby!</h1>
-  </Layout>
-)
+export const query = graphql`
+  query {
+    allMarkdownRemark(sort: { order: DESC, fields: frontmatter___date }) {
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            tags
+            date(formatString: "YYYY年MM月DD日")
+          }
+        }
+      }
+    }
+  }
+`
+
+const Body = styled.div`
+  padding: 2em;
+  ul {
+    list-style: none;
+    padding: 0px;
+  }
+  li {
+    padding: 1em;
+    margin-bottom: 1em;
+    box-shadow: 0px 0px 4px rgba(0, 0, 0, 0.3);
+    div {
+      padding: 0.25em;
+    }
+    span {
+      display: inline-block;
+      padding: 0.25em;
+      background: var(--theme-color);
+      color: white;
+      margin: 0 0.25em 0.25em 0;
+    }
+    a {
+      text-decoration: none;
+      color: black;
+    }
+  }
+`
+
+
+const IndexPage = ({ data }) => {
+
+  // query結果を取得する
+  const edges = data.allMarkdownRemark.edges
+
+  // renderする
+  return (
+    <React.Fragment>
+      <Header />
+      <Body>
+        <h1>全ての記事</h1>
+        <ul>
+          {edges.map((edge) => (
+            <li key={edge.node.id}>
+              <Link to={edge.node.id}>
+                <div>
+                  {edge.node.frontmatter.tags.map((tag) => (
+                    <span key={tag}>{tag}</span>
+                  ))}
+                </div>
+                <div>{edge.node.frontmatter.title}</div>
+                <div>{edge.node.frontmatter.date}</div>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </Body>
+      <Footer />
+    </React.Fragment>
+  )
+}
 
 export default IndexPage
